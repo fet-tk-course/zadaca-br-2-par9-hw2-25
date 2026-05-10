d91e9188621e67f3d55486ad610112a5ce15c7fd
unknown <emir.mahmutovic@fet.ba>
2026-05-10 14:15:37 +0200

    Student A Modeli

--- models_a.py
+++ models_a.py
@@ -1,5 +1,30 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
+from datetime import datetime
 
-# TODO: Student A - Definiši svoj SQLModel entitet ovdje
-# 
+
+class Enrollment(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    student_name: str
+    progress_percentage: float
+    completed: bool = False
+    enrollment_date: datetime
+    course_id: int
+
+
+class EnrollmentCreate(SQLModel):
+    student_name: str
+    progress_percentage: float
+    completed: bool = False
+    enrollment_date: datetime
+    course_id: int
+
+
+class EnrollmentUpdate(SQLModel):
+    student_name: Optional[str] = None
+    progress_percentage: Optional[float] = None
+    completed: Optional[bool] = None
+    enrollment_date: Optional[datetime] = None
+    course_id: Optional[int] = None
+
+
