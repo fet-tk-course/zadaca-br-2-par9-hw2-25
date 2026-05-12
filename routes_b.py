072d4cc101d6c75fd79af07c7e416b59dd558511
amarascic <amar.ascic@fet.ba>
2026-05-12 12:49:44 +0200

    dodano djelimicno azuriranje

--- routes_b.py
+++ routes_b.py
@@ -31,3 +31,21 @@ def read_enrollment(id: int, session: Session = Depends(get_session)):
     if not enrollment:
         raise HTTPException(status_code=404, detail="Enrollment not found")
     return enrollment
+
+
+@router.patch("/{id}", response_model=Enrollment)
+def update_enrollment(id: int, enrollment_update: EnrollmentUpdate, session: Session = Depends(get_session)):
+    db_enrollment = session.get(Enrollment, id)
+    if not db_enrollment:
+        raise HTTPException(status_code=404, detail="Enrollment not found")
+    
+
+    update_data = enrollment_update.model_dump(exclude_unset=True)
+    
+    for key, value in update_data.items():
+        setattr(db_enrollment, key, value)
+    
+    session.add(db_enrollment)
+    session.commit()
+    session.refresh(db_enrollment)
+    return db_enrollment
