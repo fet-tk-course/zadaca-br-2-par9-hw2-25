fae6eab067666431e21298a76149ef3dc3f7233f
amarascic <amar.ascic@fet.ba>
2026-05-12 12:43:32 +0200

    Dodani modeli za create i update enrollment

--- models_b.py
+++ models_b.py
@@ -2,8 +2,6 @@ from sqlmodel import SQLModel, Field
 from typing import Optional
 from datetime import datetime
 
-# TODO: Student B - Definiši svoj SQLModel entitet ovdje
-# 
 
 class Enrollment(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -13,3 +11,17 @@ class Enrollment(SQLModel, table=True):
     is_completed: bool = False
     date_enrolled: datetime = Field(default_factory=datetime.now)
     course_id: int = Field(foreign_key="course.id")
+
+
+class EnrollmentCreate(SQLModel):
+    student_name: str
+    student_email: str
+    progress: float
+    course_id: int
+
+class EnrollmentUpdate(SQLModel):
+    student_name: Optional[str] = None
+    student_email: Optional[str] = None
+    progress: Optional[float] = None
+    is_completed: Optional[bool] = None
+    course_id: Optional[int] = None
